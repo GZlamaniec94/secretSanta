8452963b65020b2c6899bfa8adff7532b0e30acb
Grzegorz Złamaniec <g.zlamaniec@gmail.com>
2022-06-07 12:06:24 +0200

    Updated views and models

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,5 +1,7 @@
 class MembersController < ApplicationController
-  before_action :set_current_group_id, only: [:create]
+  before_action :set_current_group_id, only: [:create, :destroy]
+  before_action :set_member, only: [:destroy]
+
   def new
     @member = Member.new
   end 
@@ -7,15 +9,26 @@ class MembersController < ApplicationController
   def show 
   end  
   
+  def index
+   @members = Member.all
+  end  
 
   def create
     @member = Member.new(member_params)
     @member.group_id = session[:current_group_id]    
     if @member.save
       redirect_to group_path(Group.find_by_id(session[:current_group_id]))
-    end  
+    else
+      render 'new'
+    end    
   end  
   
+  def destroy
+    @member.destroy
+    redirect_to group_path(Group.find_by_id(session[:current_group_id]))
+  end  
+
+
   private 
   def member_params
     params.require(:member).permit(:name, :email)
--- app/models/member.rb
+++ app/models/member.rb
@@ -2,10 +2,8 @@ class Member < ApplicationRecord
   before_save { self.email = email.downcase }
   belongs_to :group
   validates :name, presence: true, 
-                       uniqueness: { case_sensitive: false },
                        length: {minimum: 3, maximum: 20}
   validates :email, presence: true, 
-                    uniqueness: { case_sensitive: false },
                     length: {maximum: 105},
                     format: {with: /\A\S+@.+\.\S+\z/}
 end  
